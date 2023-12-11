264021e49069817a2c5d27483894e4d277b827c1
lazy_owl <slimsly11@gmail.com>
2023-12-11 09:52:51 +0600

    feat(kafka): if there is not given topics for consumer group - throw exception

--- pkg/kafka/consumer/static.go
+++ pkg/kafka/consumer/static.go
@@ -4,6 +4,7 @@ import (
 	"github.com/lowl11/boost2"
 	"github.com/lowl11/boost2/internal/kafka/consumer"
 	"github.com/lowl11/boost2/internal/kafka/consumer_group"
+	"github.com/lowl11/boost2/log"
 	"github.com/lowl11/boost2/pkg/kafka/configurator"
 )
 
@@ -12,5 +13,9 @@ func New(topicName string, config *configurator.Configurator) boost2.IConsumer {
 }
 
 func NewGroup(config *configurator.Configurator, topicNames ...string) boost2.IConsumer {
+	if len(topicNames) == 0 {
+		log.Fatal("No given topic names for Kafka Consumer Group")
+	}
+
 	return consumer_group.New(config, topicNames...)
 }
