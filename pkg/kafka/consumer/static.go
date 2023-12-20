687396943d40242c895e1b016b2734f6e833c0fa
lazy_owl <slimsly11@gmail.com>
2023-12-20 19:00:18 +0600

    fix(fix): fix

--- pkg/kafka/consumer/static.go
+++ pkg/kafka/consumer/static.go
@@ -4,7 +4,6 @@ import (
 	"github.com/lowl11/boost2"
 	"github.com/lowl11/boost2/internal/kafka/consumer"
 	"github.com/lowl11/boost2/internal/kafka/consumer_group"
-	"github.com/lowl11/boost2/log"
 	"github.com/lowl11/boost2/pkg/kafka/configurator"
 )
 
@@ -12,10 +11,6 @@ func New(topicName string, config *configurator.Configurator) boost2.IConsumer {
 	return consumer.New(topicName, config)
 }
 
-func NewGroup(config *configurator.Configurator, topicNames ...string) boost2.IConsumer {
-	if len(topicNames) == 0 {
-		log.Fatal("No given topic names for Kafka Consumer Group")
-	}
-
-	return consumer_group.New(config, topicNames...)
+func NewGroup(topicName string, config *configurator.Configurator) boost2.IConsumer {
+	return consumer_group.New(topicName, config)
 }
